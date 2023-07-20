1aacdf0e75fe137cd13a441737bc40ffd6771ea9
ifreewolf <ifreewolf@163.com>
2023-07-20 15:08:34 +0800

    HelloWorld python实现。
    Signed-off-by: ifreewolf <ifreewolf@163.com>

---
+++ Chapter1/demo01_ws/src/helloworld/scripts/helloworld_p.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+## 指定解释器
+
+# 1. 导包
+import rospy
+
+# 2. 编写主入口
+if __name__ == "__main__":
+    # 3. 初始化 ROS 节点
+    rospy.init_node("hello_p")
+    # 4. 输出日志
+    rospy.loginfo("hello world! by python:wq")
