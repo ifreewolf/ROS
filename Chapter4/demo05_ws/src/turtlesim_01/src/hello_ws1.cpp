47a63f96f4cfdcc535b2bde1f602661b2e6852c0
fgs <ifreewolf@163.com>
2023-08-22 00:00:21 +0800

    Finished Chapter4's params.
    Signed-off-by: fgs <ifreewolf@163.com>

---
+++ Chapter4/demo05_ws/src/turtlesim_01/src/hello_ws1.cpp
@@ -0,0 +1,9 @@
+#include "ros/ros.h"
+
+int main(int argc, char* argv[])
+{
+    ros::init(argc, argv, "hello_ws1");
+    ROS_INFO("demo01_ws");
+
+    return 0;
+}
---
+++ Chapter4/demo05_ws_ns/src/turtlesim/src/hello_ws2.cpp
@@ -0,0 +1,9 @@
+#include "ros/ros.h"
+
+int main(int argc, char* argv[])
+{
+    ros::init(argc, argv, "hello_ws2");
+    ROS_INFO("demo02_ws");
+
+    return 0;
+}
